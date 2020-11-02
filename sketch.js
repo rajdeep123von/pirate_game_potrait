83bf21162c404509db415664a8b5318420a95d92
rajdeep123von <59657573+rajdeep123von@users.noreply.github.com>
2020-11-02 21:05:45 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -161,7 +161,7 @@ function setup() {
   ship.velocityX = -3;
   //ship.lifetime = 350;
   
-  plr = createSprite(350,460);
+  plr = createSprite(350,height-240);
   plr.addAnimation( "running",idle_img);
   plr.scale = 2;
 
